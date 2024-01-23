5ee2bb8eeb325c3434b6d6d1ae9e9122f909557d
wansongtao <18101837209@163.com>
2024-01-23 21:52:43 +0800

    feat: 设置ant-design-vue的主题与语言

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
 import { initTheme } from '@/utils/theme'
+import { theme } from 'ant-design-vue'
+import { useSettingStore } from '@/stores/setting'
+import dayjs from 'dayjs'
 
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import 'dayjs/locale/zh-cn'
+
+const setStore = useSettingStore()
+
+dayjs.locale('zh-cn')
 initTheme()
 </script>
 
 <template>
-  <RouterView />
+  <a-config-provider
+    :theme="{
+      algorithm: setStore.theme === 'light' ? theme.defaultAlgorithm : theme.darkAlgorithm
+    }"
+    :locale="zhCN"
+  >
+    <RouterView />
+  </a-config-provider>
 </template>
 
 <style scoped></style>
