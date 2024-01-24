a6fcd8edbee9da69b67a03b3ab34b46cfe3efc60
wansongtao <18101837209@163.com>
2024-01-24 16:16:43 +0800

    feat: 使用glob导入页面组件

---
+++ src/config/routeComponents.ts
@@ -0,0 +1,12 @@
+/**
+ * @description: Automatically import page components
+ */
+const COMPONENT_MAP = import.meta.glob(['@/views/**/*.vue', '!@/views/**/components/*.vue'])
+
+for (const path in COMPONENT_MAP) {
+  const name = path.replace('/src/views', '')
+  COMPONENT_MAP[name] = COMPONENT_MAP[path]
+  delete COMPONENT_MAP[path]
+}
+
+export default COMPONENT_MAP
