fee9cf54b56b5036dbed8d46a3055864ea8ff429
wansongtao <18101837209@163.com>
2024-01-24 15:35:48 +0800

    feat: 完成header组件

---
+++ src/layout/components/HeaderView.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import AvatarDropdown from './AvatarDropdown.vue';
+import BreadCrumb from './BreadCrumb.vue';
+import SwitchTheme from './SwitchTheme.vue';
+
+defineOptions({
+  name: 'HeaderView'
+})
+</script>
+
+<template>
+  <header class="header flex">
+    <div class="flex">
+      <ButtonCollapsed />
+      <BreadCrumb />
+    </div>
+    <div class="flex">
+      <ScreenFull />
+      <SwitchTheme />
+      <AvatarDropdown />
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  align-items: center;
+  justify-content: space-between;
+  box-sizing: border-box;
+  height: var(--st-header-h);
+  border-bottom: 1px solid var(--st-c-divider);
+}
+
+.flex {
+  display: flex;
+}
+</style>
