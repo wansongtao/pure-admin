5485512d65f31c3f4750fce7ca87cc55f2d49111
wansongtao <18101837209@163.com>
2024-01-24 15:41:03 +0800

    feat: 添加首页与个人信息页

---
+++ src/views/home/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+defineOptions({
+  name: 'HomeView'
+})
+</script>
+
+<template>
+  <div class="st-container">
+    index
+  </div>
+</template>
+
+<style lang="scss" scoped> 
+</style>
---
+++ src/views/profile/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+defineOptions({
+  name: 'ProfileView'
+})
+</script>
+
+<template>
+  <div>profile</div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
