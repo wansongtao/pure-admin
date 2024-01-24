c129db2a8f2aed665397aabe633db0587490dadd
wansongtao <18101837209@163.com>
2024-01-24 15:01:42 +0800

    feat: 封装切换主题组件

---
+++ src/layout/components/SwitchTheme.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { useSettingStore } from '@/stores/setting'
+
+const setStore = useSettingStore()
+const { toggleTheme, followSystemTheme } = setStore
+followSystemTheme()
+
+const checked = computed(() => setStore.theme === 'dark')
+</script>
+
+<template>
+  <BaseSwitch :value="checked" :delay="300" @onChange="toggleTheme" />
+</template>
+
+<style lang="scss" scoped></style>
