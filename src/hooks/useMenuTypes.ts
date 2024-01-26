f61947c4daf40c7f3d71d524fc2c9023d0be135f
wansongtao <18101837209@163.com>
2024-01-26 16:24:08 +0800

    perf: 获取菜单类型hooks优化

--- src/hooks/useMenuTypes.ts
+++ src/hooks/useMenuTypes.ts
@@ -1,8 +1,8 @@
 import { ref } from 'vue'
 import { getMenuTypes } from '@/api/menu'
 
+const menuTypes = ref<{ label: string; value: number }[]>([])
 export const useMenuTypes = (immediate = true) => {
-  const menuTypes = ref<{ label: string; value: number }[]>([])
   const fetchMenuTypes = async () => {
     const { data } = await getMenuTypes()
     menuTypes.value = data.map((item) => ({
