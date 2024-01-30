b8d8db9217f5da7bca5adfc4c75e8f2ba75bf6bf
wansongtao <18101837209@163.com>
2024-01-30 09:16:58 +0800

    refactor: 重构判断角色指令

--- src/directive/vRole.ts
+++ src/directive/vRole.ts
@@ -1,35 +1,20 @@
 import type { App } from 'vue'
 import { useUserStore } from '@/stores/user'
-import { checkRole } from '@/utils/authority'
+import { hasRoles } from '@/utils/authority'
 
 const vRole = (app: App) => {
   app.directive<HTMLElement, string[] | string>('role', (el, binding) => {
     const userStore = useUserStore()
-
     const roles = userStore.userInfo.roles
-    const role = binding.value
+    const needRole = binding.value
+    const or = binding.modifiers.or
 
-    let isHas = true
-    if (typeof role === 'string') {
-      isHas = checkRole(roles, role)
-    } else {
-      const or = binding.modifiers.or
-      if (or) {
-        isHas = role.some((v) => {
-          return checkRole(roles, v)
-        })
-      } else {
-        isHas = role.every((v) => {
-          return checkRole(roles, v)
-        })
-      }
-    }
-
+    const isHas = hasRoles(roles, needRole, or)
     if (!isHas) {
       el.style.display = 'none'
-      setTimeout(() => {
+      setImmediate(() => {
         el.parentNode?.removeChild(el)
-      }, 0)
+      })
     }
   })
 }
