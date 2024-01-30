2564ee994977daec958feb4f01016d330d647a88
wansongtao <18101837209@163.com>
2024-01-30 09:11:38 +0800

    feat: 添加判断多个权限/角色是否存在的方法

--- src/utils/authority.ts
+++ src/utils/authority.ts
@@ -2,8 +2,8 @@ import { ALL_PERMISSION } from '@/config/index'
 
 /**
  * 权限校验
- * @param permissions 你的权限组
- * @param needPerm 是否存在的权限
+ * @param permissions 拥有的权限组
+ * @param needPerm 是否有的权限
  * @returns
  */
 export const checkAuth = (permissions: string[], needPerm: string) => {
@@ -17,13 +17,63 @@ export const checkAuth = (permissions: string[], needPerm: string) => {
 }
 
 /**
- * 角色校验
- * @param roles 你的角色组
- * @param role 是否存在的角色
- * @returns 
+ *
+ * @param permissions 拥有的权限组
+ * @param needPermissions 是否有的权限/组
+ * @param or 默认false，false => 全部有  true => 有一个即可
+ * @returns
  */
-export const checkRole = (roles: string[], role: string) => {
+export const hasPermissions = (
+  permissions: string[],
+  needPermissions: string | string[],
+  or = false
+) => {
+  if (typeof needPermissions === 'string') {
+    return checkAuth(permissions, needPermissions)
+  }
+
+  if (or) {
+    return needPermissions.some((v) => {
+      return checkAuth(permissions, v)
+    })
+  }
+
+  return needPermissions.every((v) => {
+    return checkAuth(permissions, v)
+  })
+}
+
+/**
+ * 角色校验
+ * @param roles 拥有的角色组
+ * @param needRole 是否有的角色
+ * @returns
+ */
+export const checkRole = (roles: string[], needRole: string) => {
   return roles.some((v) => {
-    return v === role ? true : false
+    return v === needRole ? true : false
+  })
+}
+
+/**
+ *
+ * @param roles 拥有的角色组
+ * @param needRoles 是否有的角色/组
+ * @param or 默认false，false => 全部有  true => 有一个即可
+ * @returns
+ */
+export const hasRoles = (roles: string[], needRoles: string | string[], or = false) => {
+  if (typeof needRoles === 'string') {
+    return checkRole(roles, needRoles)
+  }
+
+  if (or) {
+    return needRoles.some((v) => {
+      return checkRole(roles, v)
+    })
+  }
+
+  return needRoles.every((v) => {
+    return checkRole(roles, v)
   })
 }
