c0325df99c083e0fbd6ad9d81a9eb32c79c54b19
wansongtao <18101837209@163.com>
2024-01-30 15:14:32 +0800

    feat: 添加获取菜单树hooks

---
+++ src/hooks/useMenuTree.ts
@@ -0,0 +1,52 @@
+import { ref } from 'vue'
+import { getMenuTree } from '@/api/menu'
+
+import type { IMenuTree } from '@/types/api/menu'
+
+export interface ITree {
+  label: string
+  value: number
+  type: IMenuTree['type']
+  disabled?: boolean
+  children?: ITree[]
+}
+
+export const useMenuTree = (
+  callback?: (data: IMenuTree) => { label: string; value: number; disabled?: boolean },
+  immediate = true
+) => {
+  const menuTree = ref<ITree[]>([])
+
+  const fetchMenuTree = async () => {
+    const transformData = (tree: IMenuTree[]) => {
+      return tree.map((v) => {
+        const item: ITree = { label: v.name, value: v.id, type: v.type }
+        if (callback) {
+          const res = callback(v)
+          item.label = res.label
+          item.value = res.value
+          item.disabled = res.disabled
+        }
+
+        if (v.children) {
+          item.children = transformData(v.children)
+        }
+
+        return item
+      })
+    }
+
+    const { data } = await getMenuTree()
+    menuTree.value = transformData(data)
+    return menuTree.value
+  }
+
+  if (immediate) {
+    fetchMenuTree()
+  }
+
+  return {
+    menuTree,
+    fetchMenuTree
+  }
+}
