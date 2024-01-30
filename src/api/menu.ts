8417f2e906f0b62ead3ea985d73d1b42ecb178ff
wansongtao <18101837209@163.com>
2024-01-30 15:13:55 +0800

    feat: 移除获取菜单类型接口，添加获取菜单树接口

--- src/api/menu.ts
+++ src/api/menu.ts
@@ -1,19 +1,19 @@
 import instance from '@/utils/request'
 
 import type { IBaseList, IBaseResponse } from '@/types/index'
-import type { IMenuTreeItem, IQueryMenuParam } from '@/types/api/menu';
+import type { IMenuListItem, IQueryMenuParam, IMenuTree } from '@/types/api/menu'
 
-export const getMenuTypes = () => {
-  return instance.request<any, IBaseResponse<{ id: number; name: string }[]>>({
-    url: '/admin/menu/type',
-    method: 'GET'
-  })
-}
-
-export const getMenuTree = (params: IQueryMenuParam) => {
-  return instance.request<any, IBaseResponse<IBaseList<IMenuTreeItem>>>({
+export const getMenuList = (params: IQueryMenuParam) => {
+  return instance.request<any, IBaseResponse<IBaseList<IMenuListItem>>>({
     url: '/admin/menu',
     method: 'GET',
     params
+  })
+}
+
+export const getMenuTree = () => {
+  return instance.request<any, IBaseResponse<IMenuTree[]>>({
+    url: '/admin/menu/tree',
+    method: 'GET'
   })
 }
