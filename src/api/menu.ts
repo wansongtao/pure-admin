85667abf2d3f2890531aeffb0420c96dd76e7cb1
wansongtao <18101837209@163.com>
2024-01-31 11:13:59 +0800

    feat: 新增添加菜单接口

--- src/api/menu.ts
+++ src/api/menu.ts
@@ -1,7 +1,7 @@
 import instance from '@/utils/request'
 
 import type { IBaseList, IBaseResponse } from '@/types/index'
-import type { IMenuListItem, IQueryMenuParam, IMenuTree } from '@/types/api/menu'
+import type { IMenuListItem, IQueryMenuParam, IMenuTree, IMenuParam } from '@/types/api/menu'
 
 export const getMenuList = (params: IQueryMenuParam) => {
   return instance.request<any, IBaseResponse<IBaseList<IMenuListItem>>>({
@@ -15,5 +15,13 @@ export const getMenuTree = () => {
   return instance.request<any, IBaseResponse<IMenuTree[]>>({
     url: '/admin/menu/tree',
     method: 'GET'
+  })
+}
+
+export const addMenu = (data: IMenuParam) => {
+  return instance.request<any, IBaseResponse>({
+    url: '/admin/menu',
+    method: 'post',
+    data
   })
 }
