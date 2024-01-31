d03aaee15460d46a4408be685d998730cdb604e8
wansongtao <18101837209@163.com>
2024-01-31 11:16:29 +0800

    feat: 新增菜单相关字段校验方法

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -27,3 +27,56 @@ export const validatePassword = async (_rule: Rule, value: string) => {
 
   return Promise.resolve()
 }
+
+export const validateMenuTitle = async (_rule: Rule, value: string) => {
+  const regexp = /^[a-zA-Z\u4e00-\u9fa5]{2,16}$/
+  if (!regexp.test(value)) {
+    return Promise.reject(
+      '请输入2至16位由中文、字母组成的名称'
+    )
+  }
+
+  return Promise.resolve()
+}
+
+export const validateMenuPath = async (_rule: Rule, value: string) => {
+  const regexp = /^[a-z/:]{2,30}$/
+  if (!regexp.test(value)) {
+    return Promise.reject(
+      '请输入2至30位由小写字母、‘/’、‘:’组成的菜单路径'
+    )
+  }
+
+  return Promise.resolve()
+}
+
+export const validateMenuComponent = async (_rule: Rule, value: string) => {
+  const regexp = /^\/[a-zA-Z/.]{6,100}$/
+  if (!regexp.test(value)) {
+    return Promise.reject(
+      '请输入6至100位由字母、‘/’、‘.’组成的组件路径，必须以’/‘开头(views文件夹下路径)'
+    )
+  }
+
+  return Promise.resolve()
+}
+
+export const validateMenuPermission = async (_rule: Rule, value: string) => {
+  const regexp = /^[a-z:]{1,50}$/
+  if (!regexp.test(value)) {
+    return Promise.reject(
+      '请输入1至50位由字母、‘:’组成的菜单权限'
+    )
+  }
+
+  return Promise.resolve()
+}
+
+export const validateMenuRedirect = async (_rule: Rule, value: string) => {
+  const regexp = /^[a-zA-Z/0-9]{2,50}$/
+  if (!regexp.test(value)) {
+    return Promise.reject('请输入2至50位由小写字母、数字、‘/’组成的重定向地址')
+  }
+
+  return Promise.resolve()
+}
