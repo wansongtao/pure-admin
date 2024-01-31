5ff67f296dc631c3ffb6a0e880a8aa8bb0503698
wansongtao <18101837209@163.com>
2024-01-31 11:21:12 +0800

    feat: 新增添加菜单按钮组件

---
+++ src/views/system/menu/components/ButtonAdd.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import MenuModal from './MenuModal.vue';
+
+import { addMenu } from '@/api/menu'
+import { message } from 'ant-design-vue';
+import { removeObjectEmptyProto } from '@/utils/index'
+
+import type { IMenuParam } from '@/types/api/menu'
+
+const $emits = defineEmits<{
+  addSuccess: []
+}>()
+
+const open = ref(false)
+const handleOpen = () => {
+  open.value = true
+}
+
+const modalRef = ref<InstanceType<typeof MenuModal>>()
+const handleAdd = (param: IMenuParam) => {
+  const data = removeObjectEmptyProto(param)
+
+  addMenu(data).then(() => {
+    open.value = false
+    modalRef.value?.resetForm()
+    message.success('添加菜单成功')
+    $emits('addSuccess')
+  })
+}
+</script>
+
+<template>
+  <a-button type="primary" @click="handleOpen">新增</a-button>
+  <menu-modal ref="modalRef" v-model="open" @verify-success="handleAdd" />
+</template>
+
+<style lang="scss" scoped>
+
+</style>
