fd21853a209198cb891cebd611ce6a4631c41c0e
wansongtao <18101837209@163.com>
2024-02-01 16:25:05 +0800

    feat: 新增菜单删除功能组件

---
+++ src/views/system/menu/components/MenuDelete.vue
@@ -0,0 +1,62 @@
+<script lang="ts" setup>
+import { message } from 'ant-design-vue'
+import { deleteMenu, deleteMultipleMenus } from '@/api/menu'
+
+const $props = defineProps<{
+  id: number | number[]
+}>()
+const $emits = defineEmits<{
+  handleSuccess: []
+}>()
+
+const isSingle = computed(() => {
+  return typeof $props.id === 'number' ? true : false
+})
+
+const beforeOpenConfirm = () => {
+  if (typeof $props.id === 'number') {
+    return true
+  }
+
+  if (!$props.id?.length) {
+    message.warn('请先选中删除项')
+    return false
+  }
+
+  return true
+}
+
+const handleDelete = () => {
+  const success = (text = '删除菜单成功') => {
+    message.success(text)
+    $emits('handleSuccess')
+  }
+
+  const id = $props.id
+  if (typeof id === 'number') {
+    deleteMenu(id).then(() => {
+      success()
+    })
+    return
+  }
+
+  deleteMultipleMenus(id).then(() => {
+    success('批量删除菜单成功')
+  })
+}
+</script>
+
+<template>
+  <ButtonDelete
+    :size="isSingle ? 'small' : 'middle'"
+    :text="isSingle ? '删除' : '批量删除'"
+    :confirm-config="{
+      title: isSingle ? '您确定要删除这条数据吗？' : '您确定要删除这些数据吗？'
+    }"
+    :ghost="isSingle"
+    :before-open-confirm="beforeOpenConfirm"
+    @handle-ok="handleDelete"
+  />
+</template>
+
+<style lang="scss" scoped></style>
