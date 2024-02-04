114c81704600e6638ac54d8902d307630b7d0b0a
wansongtao <18101837209@163.com>
2024-02-04 09:52:37 +0800

    feat: 完成角色列表页的查询与排序功能

---
+++ src/views/system/role/components/TFilter.vue
@@ -0,0 +1,60 @@
+<script lang="ts" setup>
+import { STATUS } from '@/config/index'
+
+import type { IBaseQuery } from '@/types/index'
+import type { IRoleQuery } from '@/types/api/role'
+
+const $emits = defineEmits<{
+  handleSearch: [query: IRoleQuery]
+  handleReset: []
+}>()
+
+const disabled = ref<IRoleQuery['disabled']>(undefined)
+
+const handleSearch = (data: IBaseQuery) => {
+  const params: IRoleQuery = {}
+  if (data?.keyword) {
+    params.name = data.keyword
+  }
+  if (data?.startTime) {
+    params.startTime = data.startTime
+  }
+  if (data?.endTime) {
+    params.endTime = data.endTime
+  }
+  if (disabled.value !== undefined) {
+    params.disabled = disabled.value
+  }
+
+  $emits('handleSearch', params)
+}
+const handleReset = () => {
+  disabled.value = undefined
+
+  $emits('handleReset')
+}
+</script>
+
+<template>
+  <BaseFilter
+    :span="8"
+    :disabled-empty-search="disabled === undefined"
+    @handle-search="handleSearch"
+    @handle-reset="handleReset"
+  >
+    <template #default>
+      <a-col :span="8">
+        <a-select
+          v-model:value="disabled"
+          style="width: 100%"
+          placeholder="角色状态"
+          allow-clear
+          :options="STATUS"
+        ></a-select>
+      </a-col>
+    </template>
+  </BaseFilter>
+</template>
+
+<style lang="scss" scoped></style>
+ 
--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -1,13 +1,102 @@
 <script lang="ts" setup>
+import TFilter from './components/TFilter.vue'
+
+import { usePageRequest } from '@/hooks/usePageRequest'
+import { getRoleList } from '@/api/role'
+
+import type { IRoleQuery, IRoleList } from '@/types/api/role'
+import type { TableColumnProps } from 'ant-design-vue'
+
 defineOptions({
   name: 'systemRole'
 })
+
+const requestData = async (params: IRoleQuery) => {
+  const { result } = await getRoleList(params)
+
+  return {
+    data: result?.data.list ?? [],
+    total: result?.data.total ?? 0
+  }
+}
+
+const columns: (TableColumnProps & { dataIndex?: keyof IRoleList })[] = [
+  {
+    align: 'center',
+    title: '角色ID',
+    dataIndex: 'id',
+    width: 100
+  },
+  {
+    align: 'center',
+    title: '角色名称',
+    dataIndex: 'name'
+  },
+  {
+    align: 'center',
+    title: '角色描述',
+    dataIndex: 'description',
+    ellipsis: true
+  },
+  {
+    align: 'center',
+    title: '是否禁用',
+    dataIndex: 'disabled'
+  },
+  {
+    align: 'center',
+    title: '添加时间',
+    dataIndex: 'createTime',
+    sorter: true,
+    width: 180
+  },
+  {
+    align: 'center',
+    title: '操作',
+    key: 'operation',
+    fixed: 'right',
+    width: 160
+  }
+]
+
+const { page, pageSize, total, loading, list, getList } = usePageRequest(requestData)
+
+const query = ref<IRoleQuery>({})
+const handleQuery = (data?: IRoleQuery) => {
+  query.value = data ?? {}
+
+  getList(query.value)
+}
+
+const handleSort = (fieldName: keyof IRoleList, order?: 'descend' | 'ascend' | null) => {
+  if (fieldName === 'createTime') {
+    query.value.isDesc = order === 'ascend' ? 0 : 1
+    getList(query.value)
+    return
+  }
+}
 </script>
 
 <template>
-  <div>role</div>
+  <div class="st-container">
+    <t-filter :loading="loading" @handle-search="handleQuery" @handle-reset="handleQuery" />
+
+    <base-table
+      :columns="columns"
+      :default-show-operation="false"
+      :loading="loading"
+      :list="list"
+      @handle-sort="handleSort"
+    >
+      <template #default="{ column, record }">
+        <template v-if="column.dataIndex === 'disabled'">
+          {{ record.disabled ? '是' : '否' }}
+        </template>
+      </template>
+    </base-table>
+
+    <base-pagination v-model:page="page" v-model:page-size="pageSize" :total="total" />
+  </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
