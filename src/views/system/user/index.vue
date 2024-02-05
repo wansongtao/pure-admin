7822033d84607cb794d1576e3fd06201346e6dd0
wansongtao <18101837209@163.com>
2024-02-05 11:29:39 +0800

    feat: 完成用户管理页列表分页查询功能开发

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -1,13 +1,96 @@
 <script lang="ts" setup>
+import { usePageRequest } from '@/hooks/usePageRequest'
+import { getUserList } from '@/api/user'
+
+import type { IUserQuery, IUserList } from '@/types/api/user'
+import type { TableColumnProps } from 'ant-design-vue'
+
 defineOptions({
   name: 'systemUser'
 })
+
+const requestData = async (params: IUserQuery) => {
+  const { result } = await getUserList(params)
+
+  return {
+    data: result?.data.list ?? [],
+    total: result?.data.total ?? 0
+  }
+}
+
+const columns: (TableColumnProps & { dataIndex?: keyof IUserList })[] = [
+  {
+    align: 'center',
+    title: '用户ID',
+    dataIndex: 'id',
+    width: 100
+  },
+  {
+    align: 'center',
+    title: '用户名',
+    dataIndex: 'userName'
+  },
+  {
+    align: 'center',
+    title: '角色头像',
+    dataIndex: 'avatar'
+  },
+  {
+    align: 'center',
+    title: '用户昵称',
+    dataIndex: 'nickName'
+  },
+  {
+    align: 'center',
+    title: '是否禁用',
+    dataIndex: 'disabled'
+  },
+  {
+    align: 'center',
+    title: '添加时间',
+    dataIndex: 'createTime',
+    sorter: true,
+    width: 180
+  },
+  {
+    align: 'center',
+    title: '操作',
+    key: 'operation',
+    fixed: 'right',
+    width: 160
+  }
+]
+
+const { page, pageSize, total, loading, list, getList, lastPage } = usePageRequest(requestData)
 </script>
 
 <template>
-  <div>user</div>
+  <div class="st-container">
+    <base-table :columns="columns" :default-show-operation="false" :loading="loading" :list="list">
+      <template #default="{ column, record }">
+        <template v-if="column.dataIndex === 'avatar'">
+          <div class="avatar">
+            <img class="avatar_img" :src="record.avatar" alt="" />
+          </div>
+        </template>
+      </template>
+    </base-table>
+
+    <base-pagination v-model:page="page" v-model:page-size="pageSize" :total="total" />
+  </div>
 </template>
 
 <style lang="scss" scoped>
+.avatar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
 
+  .avatar_img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
+}
 </style>
