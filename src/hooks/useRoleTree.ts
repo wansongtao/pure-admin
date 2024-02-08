296ff9aed7f721dedc9449f0edd5102cb7e9e24c
wansongtao <18101837209@163.com>
2024-02-08 20:15:02 +0800

    feat: 封装获取角色hooks

---
+++ src/hooks/useRoleTree.ts
@@ -0,0 +1,25 @@
+import { ref } from 'vue'
+import { getRoleTree } from '@/api/role'
+
+import type { IRoleTree } from '@/types/api/role'
+
+export const useRoleTree = (immediate = true) => {
+  const roleTree = ref<IRoleTree[]>([])
+
+  const fetchRoleTree = async () => {
+    const { result } = await getRoleTree()
+    if (result) {
+      roleTree.value = result?.data
+    }
+    return roleTree.value
+  }
+
+  if (immediate) {
+    fetchRoleTree()
+  }
+
+  return {
+    roleTree,
+    fetchRoleTree
+  }
+}
