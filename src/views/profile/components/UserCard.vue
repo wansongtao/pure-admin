a20b48c22ff08ad3151fa61b307cc2fa25506bef
wansongtao <18101837209@163.com>
2024-02-22 11:28:01 +0800

    feat: 完成个人中心的用户信息展示卡片模块

---
+++ src/views/profile/components/UserCard.vue
@@ -0,0 +1,121 @@
+<script lang="ts" setup>
+import UserIcon from '@/assets/svg/menus/user.svg'
+import RoleIcon from '@/assets/svg/menus/role.svg'
+import PhoneIcon from '@/assets/svg/phone.svg'
+import EmailIcon from '@/assets/svg/menus/email.svg'
+import SexmIcon from '@/assets/svg/sexm.svg'
+import SexwIcon from '@/assets/svg/sexw.svg'
+import SexIcon from '@/assets/svg/intersex.svg'
+import BirthdayIcon from '@/assets/svg/birthday.svg'
+import UploadAvatar from '@/components/UploadAvatar/index.vue'
+
+import { updateProfile } from '@/api/common'
+import { message } from 'ant-design-vue'
+
+import type { IProfile } from '@/types/api/common'
+
+const $props = defineProps<{ user: IProfile }>()
+
+const avatar = ref('')
+watch(
+  () => $props.user.avatar,
+  (url) => {
+    if (url) {
+      avatar.value = url
+    }
+  }
+)
+
+const avatarRef = ref<InstanceType<typeof UploadAvatar>>()
+const handleUpdateAvatar = async () => {
+  const result = await avatarRef.value?.handleUpload().catch(() => {
+    message.error('头像上传失败')
+    return
+  })
+  if (!result) {
+    return
+  }
+
+  const { error } = await updateProfile({ avatar: result })
+  if (error) {
+    return
+  }
+
+  message.success('头像修改成功')
+}
+</script>
+
+<template>
+  <a-card>
+    <div class="flex_center">
+      <upload-avatar ref="avatarRef" v-model:imgUrl="avatar" @selectFile="handleUpdateAvatar" />
+    </div>
+    <p class="name">{{ user.nickName || user.userName }}</p>
+    <p class="text" v-if="user.description">{{ user.description }}</p>
+    <div class="profile_main">
+      <div class="main_item text">
+        <div class="item_label">
+          <sex-icon v-if="user.sex !== 0 && user.sex !== 1" />
+          <sexw-icon v-if="user.sex === 0" />
+          <sexm-icon v-if="user.sex === 1" />
+        </div>
+        <div class="item_text">{{ user.sex === 0 ? '女' : user.sex === 1 ? '男' : '--' }}</div>
+      </div>
+      <div class="main_item text">
+        <div class="item_label"><user-icon /></div>
+        <div class="item_text">{{ user.userName }}</div>
+      </div>
+      <div class="main_item text">
+        <div class="item_label"><role-icon /></div>
+        <div class="item_text">{{ user?.roles.join(', ') }}</div>
+      </div>
+      <div class="main_item text">
+        <div class="item_label"><birthday-icon /></div>
+        <div class="item_text">{{ user.birthday || '--' }}</div>
+      </div>
+      <div class="main_item text">
+        <div class="item_label"><phone-icon /></div>
+        <div class="item_text">{{ user.phone || '--' }}</div>
+      </div>
+      <div class="main_item text">
+        <div class="item_label"><email-icon /></div>
+        <div class="item_text">{{ user.email || '--' }}</div>
+      </div>
+    </div>
+  </a-card>
+</template>
+
+<style lang="scss" scoped>
+.name {
+  font-size: 20px;
+  line-height: 2em;
+  text-align: center;
+  color: var(--st-c-text-1);
+}
+
+.text {
+  line-height: 1.7em;
+  text-align: center;
+  color: var(--st-c-text-1);
+}
+
+.profile_main {
+  padding-top: 15px;
+  margin: 0 auto;
+  width: 60%;
+}
+
+.main_item {
+  display: flex;
+
+  .item_label {
+    margin-right: 15px;
+    flex-shrink: 0;
+  }
+
+  .item_text {
+    flex: 1;
+    text-align: left;
+  }
+}
+</style>
