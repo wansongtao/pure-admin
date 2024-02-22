758bb18215c969b065f68d2e3d6051bdf974108b
wansongtao <18101837209@163.com>
2024-02-22 16:56:48 +0800

    feat: 完成个人中心页面开发

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,13 +1,74 @@
 <script lang="ts" setup>
+import UserCard from './components/UserCard.vue'
+import UserInfoForm from './components/UserInfoForm.vue'
+import PasswordForm from './components/PasswordForm.vue'
+
+import { getProfile } from '@/api/common'
+
+import type { IProfile, IProfileParam } from '@/types/api/common'
+
 defineOptions({
   name: 'ProfileView'
 })
+
+const profile = ref<IProfile>({
+  userName: '',
+  roles: [],
+  sex: 2
+})
+
+const handleChangeProfile = (data: IProfileParam) => {
+  profile.value = { ...profile.value, ...data }
+}
+
+const requestProfile = async () => {
+  const { result } = await getProfile()
+  if (!result) {
+    return
+  }
+  profile.value = result.data
+}
+requestProfile()
+
+type IKey = 'info' | 'pwd'
+const tabList: { key: IKey; tab: string }[] = [
+  { key: 'info', tab: '个人信息修改' },
+  { key: 'pwd', tab: '密码修改' }
+]
+const key = ref<IKey>('info')
+const onTabChange = (value: string) => {
+  key.value = value as IKey
+}
 </script>
 
 <template>
-  <div>profile</div>
+  <div class="profile">
+    <a-row :gutter="20">
+      <a-col :span="8">
+        <user-card :user="profile" />
+      </a-col>
+      <a-col :span="16">
+        <a-card
+          style="width: 100%"
+          :tab-list="tabList"
+          :active-tab-key="key"
+          @tabChange="(key) => onTabChange(key)"
+        >
+          <user-info-form
+            v-show="key === 'info'"
+            :details="profile"
+            @on-change="handleChangeProfile"
+          />
+
+          <password-form v-show="key === 'pwd'" />
+        </a-card>
+      </a-col>
+    </a-row>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-
+.profile {
+  padding: 20px;
+}
 </style>
