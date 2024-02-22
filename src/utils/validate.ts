8d9df09cae567c420da7747dcb72c68395753d8f
wansongtao <18101837209@163.com>
2024-02-22 10:32:52 +0800

    feat: 新增校验手机号、邮箱格式方法

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -116,3 +116,29 @@ export const validateNickName = async (_rule: Rule, value: string) => {
 
   return Promise.resolve()
 }
+
+export const validatePhone = async (rule: Rule, value: string) => {
+  if (!rule.required && value === '') {
+    return Promise.resolve()
+  }
+  
+  const regexp = /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/
+  if (!regexp.test(value)) {
+    return Promise.reject('手机号格式错误')
+  }
+
+  return Promise.resolve()
+}
+
+export const validateEmail = async (rule: Rule, value: string) => {
+  if (!rule.required && value === '') {
+    return Promise.resolve()
+  }
+  
+  const regexp = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+  if (!regexp.test(value)) {
+    return Promise.reject('邮箱格式错误')
+  }
+
+  return Promise.resolve()
+}
