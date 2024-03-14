6b4de8eaf8085f90fd5cb6dcf835435c34d48512
wansongtao <18101837209@163.com>
2024-03-14 14:41:43 +0800

    refactor: 重构axios请求/响应拦截器逻辑，发送多个相同请求时，仅向服务器发送一个请求，多个请求共享响应结果

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,61 +1,10 @@
-import axios, { type AxiosError, type AxiosResponse, type AxiosRequestConfig } from 'axios'
+import axios, { AxiosError, type AxiosResponse, type AxiosRequestConfig } from 'axios'
 import { message } from 'ant-design-vue'
 import { useUserStore } from '@/stores/user'
-import { getDataType, throttle } from '@/utils/index'
+import { getDataType } from '@/utils/index'
+import EventBus from '@/event/eventBus'
 
 import type { IBaseResponse } from '@/types/index'
-
-const historyRequestMap: Record<string, number> = {}
-/**
- * Async clear expired request information every 5 seconds.
- */
-const clearHistoryReqMap = throttle((expiredTime: number) => {
-  setTimeout(() => {
-    const keys = Object.keys(historyRequestMap)
-    if (!keys.length) {
-      return
-    }
-
-    for (const key in keys) {
-      if (historyRequestMap[key] < expiredTime) {
-        delete historyRequestMap[key]
-      }
-    }
-  }, 0)
-}, 5000)
-const isDuplicateRequest = (config: AxiosRequestConfig, interval = 400) => {
-  const { method, url, data, params } = config
-  let key = `${method}-${url}`
-
-  try {
-    if (data && getDataType(data) === 'object') {
-      key += `-${JSON.stringify(data)}`
-    }
-    if (params && getDataType(params) === 'object') {
-      key += `-${JSON.stringify(params)}`
-    }
-
-    const timestamp = Date.now()
-    if (historyRequestMap[key] && timestamp - historyRequestMap[key] < interval) {
-      return true
-    }
-
-    historyRequestMap[key] = timestamp
-    clearHistoryReqMap(timestamp - interval)
-    return false
-  } catch (e) {
-    return false
-  }
-}
-
-const instance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_API,
-  timeout: 5000,
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json;'
-  }
-})
 
 const goToLogin = (seconds = 2) => {
   setTimeout(() => {
@@ -67,20 +16,53 @@ const goToLogin = (seconds = 2) => {
   }, seconds * 1000)
 }
 
-const createError = (message = 'error', code = 'api error'): AxiosError => {
-  return {
-    code,
-    message,
-    name: '',
-    isAxiosError: false,
-    toJSON: () => {
-      return { '[Object]': '[Object]' }
+const getKey = (config: AxiosRequestConfig) => {
+  const { method, url, data, params } = config
+  let key = `${method}-${url}`
+
+  try {
+    if (data && getDataType(data) === 'object') {
+      key += `-${JSON.stringify(data)}`
     }
+    if (params && getDataType(params) === 'object') {
+      key += `-${JSON.stringify(params)}`
+    }
+  } catch (e) {
+    console.error(e)
   }
+
+  return key
 }
+
+const historyRequests = new Map<string, number>()
+const isRedundantRequest = (key: string) => {
+  if (historyRequests.has(key)) {
+    return true
+  }
+
+  historyRequests.set(key, 1)
+  return false
+}
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_API,
+  timeout: 5000,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json;'
+  }
+})
 
 instance.interceptors.request.use(
   (config) => {
+    // 防止重复请求
+    const key = getKey(config)
+    if (isRedundantRequest(key)) {
+      config.headers.key = key
+      config.headers.requestTime = Date.now()
+      return Promise.reject(new AxiosError('Redundant request', 'ERR_REPEATED', config))
+    }
+
     if (config.headers?.isToken !== false) {
       const store = useUserStore()
       const token = store.getToken()
@@ -90,10 +72,6 @@ instance.interceptors.request.use(
       }
     }
 
-    if (!config.headers?.allowDuplication && isDuplicateRequest(config)) {
-      return Promise.reject({ code: 'ERR_CANCELED', message: 'Duplicate request' })
-    }
-
     return config
   },
   (error: AxiosError) => {
@@ -101,27 +79,70 @@ instance.interceptors.request.use(
   }
 )
 
-instance.interceptors.response.use(
-  (res: AxiosResponse<IBaseResponse | Blob>) => {
-    const data = res.data
+const responseInterceptor = (res: AxiosResponse<IBaseResponse | Blob>) => {
+  const data = res.data
+  const result: [AxiosResponse<IBaseResponse | Blob> | undefined, AxiosError | undefined] = [
+    undefined,
+    undefined
+  ]
 
-    if (data instanceof Blob || data.code === 200) {
-      return res
-    }
-
+  if (data instanceof Blob || data.code === 200) {
+    result[0] = res
+  } else {
     if (data.code === 300 || data.code === 401) {
       message.error(data.msg, 2)
       goToLogin()
-      return Promise.reject(createError(data.msg))
+    } else {
+      message.error(data.msg)
+    }
+    result[1] = new AxiosError(data.msg)
+  }
+
+  return result
+}
+
+const eventBus = new EventBus<{
+  [key: string]: (data?: AxiosResponse<IBaseResponse | Blob>, error?: AxiosError) => void
+}>()
+
+instance.interceptors.response.use(
+  (res: AxiosResponse<IBaseResponse | Blob>) => {
+    const [data, error] = responseInterceptor(res)
+
+    // 如果存在重复请求，则触发事件，将结果返回给请求
+    const key = getKey(res.config)
+    if (historyRequests.has(key)) {
+      historyRequests.delete(key)
+      eventBus.$emit(key, data, error)
     }
 
-    message.error(data.msg)
-    return Promise.reject(createError(data.msg))
+    return data !== undefined ? data : Promise.reject(error)
   },
   (error: AxiosError) => {
-    // Cancel request
+    // 处理重复请求
+    if (error.code === 'ERR_REPEATED') {
+      return new Promise((resolve, reject) => {
+        const config = error.config!
+        const key = config.headers.key as string
+        const callback = (res?: AxiosResponse<IBaseResponse | Blob>, err?: AxiosError) => {
+          res ? resolve(res) : reject(err)
+          eventBus.$off(key, callback)
+        }
+        eventBus.$on(key, callback)
+
+        const timeout = config.timeout || 5000
+        const requestTime = config.headers.requestTime as number
+        const now = Date.now()
+        if (now - requestTime > timeout) {
+          historyRequests.delete(key)
+          const error = new AxiosError(`timeout of ${timeout}ms exceeded`, 'ECONNABORTED', config)
+          error.name = 'AxiosError'
+          eventBus.$emit(key, undefined, error)
+        }
+      })
+    }
+
     if (error.code === 'ERR_CANCELED') {
-      message.warn(error.message)
       return Promise.reject(error)
     }
 
