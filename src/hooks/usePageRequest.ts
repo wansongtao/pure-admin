e703bde1f6779e46eb4d7c9007ee0e02b2c4754d
wansongtao <18101837209@163.com>
2024-03-27 16:18:33 +0800

    refactor: 使用函数重载重构方法，提供更好的类型提示

--- src/hooks/usePageRequest.ts
+++ src/hooks/usePageRequest.ts
@@ -4,42 +4,54 @@ import { useQuery } from './useQuery'
 
 import type { Ref, ShallowRef } from 'vue'
 
-/**
- * 分页请求数据
- * @param requestData
- * @param param1
- * @returns
- */
-export const usePageRequest = <
-  T = unknown,
-  K extends Ref<T[]> | ShallowRef<T[]> = Ref<T[]>,
-  Q extends Record<string, any> = { page: number; pageSize: number }
->(
-  requestData: (query: Q) => Promise<{ data: T[]; total: number }>,
+export interface IPageRequestOptions {
+  defaultPage?: number
+  defaultPageSize?: number
+  /**
+   * 请求防抖时间，单位ms，设置为0则不防抖
+   */
+  delay?: number
+  /**
+   * 是否自动监听page与pageSize的变化
+   */
+  autoWatchPage?: boolean
+}
+export type IRequestFn<T, Q> = (query: Q) => Promise<{ data: T[]; total: number }>
+interface IPageRequestReturn<Q> {
+  total: Ref<number>
+  loading: Ref<boolean>
+  page: Ref<number>
+  pageSize: Ref<number>
+  lastPage: Ref<number>
+  getList: (query?: Q) => void
+}
+
+export function usePageRequest<T, Q extends Record<string, any> = {}>(
+  requestFn: IRequestFn<T, Q>,
+  options?: IPageRequestOptions
+): {
+  list: Ref<T[]>
+} & IPageRequestReturn<Q>
+
+export function usePageRequest<T, Q extends Record<string, any> = {}>(
+  requestFn: IRequestFn<T, Q>,
+  options: IPageRequestOptions,
+  isShallow: true
+): {
+  list: ShallowRef<T[]>
+} & IPageRequestReturn<Q>
+
+export function usePageRequest<T, Q extends Record<string, any> = {}>(
+  requestFn: IRequestFn<T, Q>,
   {
     defaultPage = 1,
     defaultPageSize = 10,
-    deep = true,
     delay = 200,
     autoWatchPage = false
-  }: {
-    defaultPage?: number
-    defaultPageSize?: number
-    /**
-     * true：对整个列表进行响应式，false：仅监听列表长度的变化 (默认 true)
-     */
-    deep?: boolean
-    /**
-     * 防抖时间，单位ms
-     */
-    delay?: number
-    /**
-     * 是否自动监听page与pageSize的变化
-     */
-    autoWatchPage?: boolean
-  } = {}
-) => {
-  const list: K = (deep ? ref([]) : shallowRef([])) as unknown as K
+  }: IPageRequestOptions = {},
+  isShallow = false
+) {
+  const list: Ref<T[]> | ShallowRef<T[]> = !isShallow ? ref([]) : shallowRef([])
   const loading = ref(false)
   const total = ref(0)
   const page = useQuery('page', defaultPage, { transform: Number })
@@ -53,14 +65,11 @@ export const usePageRequest = <
     return Math.ceil(total.value / pageSize.value)
   })
 
-  /**
-   * 请求列表数据，自动带上page与pageSize，只传入其他query即可
-   */
-  const getList: (query?: Q) => void = debounce((query?: Q) => {
+  const fetchData = (query?: Q) => {
     const params = { page: page.value, pageSize: pageSize.value, ...query } as unknown as Q
     loading.value = true
-    
-    requestData(params)
+
+    requestFn(params)
       .then((res) => {
         list.value = res.data
         total.value = res.total
@@ -68,7 +77,11 @@ export const usePageRequest = <
       .finally(() => {
         loading.value = false
       })
-  }, delay ?? 0)
+  }
+  /**
+   * 请求列表数据，自动带上page与pageSize，只传入其他query即可
+   */
+  const getList: (query?: Q) => void = delay ? debounce(fetchData, delay) : fetchData
 
   if (autoWatchPage) {
     watch(
